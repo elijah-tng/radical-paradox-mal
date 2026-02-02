95bd2ea4f46d943d20a6b646fe11df102333b852
tripleo <deb12@d250504z.tripleo.milkyway>
2026-02-02 02:16:16 -0500

    Shadowed

--- src/main/java/tripleo/elijah_prolific/comp_signals/CSS2_Advisable.java
+++
@@ -1,5 +0,0 @@
-package tripleo.elijah_prolific.comp_signals;
-
-public interface CSS2_Advisable {
-    void adviseObject(Object aPayload);
-}
--- src/main/java/tripleo/elijah_prolific/comp_signals/CSS2_Signal.java
+++
@@ -1,7 +0,0 @@
-package tripleo.elijah_prolific.comp_signals;
-
-import tripleo.elijah.comp.*;
-
-public interface CSS2_Signal {
-    void trigger(Compilation compilation, Object payload);
-}
